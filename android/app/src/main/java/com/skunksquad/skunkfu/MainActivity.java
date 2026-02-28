081f0c6ff0cfed6867158468c3e01f3e169a3924
marcfabel <marcfabel@gmail.com>
2026-02-28 22:22:27 +0000

    firebas update to add ad banner

--- android/app/src/main/java/com/skunksquad/skunkfu/MainActivity.java
+++ android/app/src/main/java/com/skunksquad/skunkfu/MainActivity.java
@@ -1,5 +1,29 @@
 package com.skunksquad.skunkfu;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends BridgeActivity {
+    private AdView mAdView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
